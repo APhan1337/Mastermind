b901118eacaa86ac688b98ad314352fd3bce6f0d
Alex Phan <alexphan@alexs-air.lan>
2023-01-06 16:31:12 -0800

    Switch to SwiftUI

---
+++ Mastermind/MastermindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MastermindApp.swift
+//  Mastermind
+//
+//  Created by Alex Phan on 1/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MastermindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
